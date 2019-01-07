d901f7b823a5ef1e44535cbeb3a2b7ccfbc7dccd
Brian J. Cardiff <bcardiff@gmail.com>
2019-01-07 17:41:35 -0300

    Add function to support coroutines with their own stack

--- pthread_support.c
+++ pthread_support.c
@@ -1369,6 +1369,45 @@ GC_INNER void GC_do_blocking_inner(ptr_t data, void * context GC_ATTR_UNUSED)
     UNLOCK();
 }
 
+/* Inform a context switch of a coroutine in the current thread.         */
+/* There is no registry of all the existing corrutines so               */
+/* GC_push_all_eager() must be called for all other paused corrutines.  */
+/* MUST be called with the GC disabled.                                 */
+GC_API void GC_CALL GC_switch_to_coroutine(char * stackbottom)
+{
+    pthread_t self = pthread_self();
+    GC_thread me = GC_lookup_thread(self);
+
+    if ((me -> flags & MAIN_THREAD) == 0) {
+        me -> stack_end = stackbottom;
+    } else {
+        GC_stackbottom = stackbottom;
+    }
+}
+
+/* Returns the cool end of user stack of the current thread/corutine.   */
+/* When a thread starts it will be computed upon creation.              */
+/* If GC_switch_to_coroutine() is called, the returned value will match */
+/* the specified value in that function.                                 */
+GC_API char * GC_CALL GC_get_stackbottom()
+{
+    pthread_t self = pthread_self();
+    GC_thread me;
+    char * ret;
+
+    LOCK();
+    me = GC_lookup_thread(self);
+
+    if ((me -> flags & MAIN_THREAD) == 0) {
+        ret = me -> stack_end;
+    } else {
+        ret = GC_stackbottom;
+    }
+    UNLOCK();
+
+    return ret;
+}
+
 /* GC_call_with_gc_active() has the opposite to GC_do_blocking()        */
 /* functionality.  It might be called from a user function invoked by   */
 /* GC_do_blocking() to temporarily back allow calling any GC function   */
